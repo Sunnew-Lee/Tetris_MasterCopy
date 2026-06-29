d6ec7dd2705a731ecf0edd6b7a7c335378f6c9cf
Sunwoo Lee <sunwoo9078@gmail.com>
2026-06-29 22:02:50 +0900

    Fix unplayable bug
    원인: 코드가 아니라 p5.js 웹 에디터(editor.p5js.org)의 "무한 루프 방지(loop-protect)" 기능 버그입니다.
    
    _LP7처럼 매번 숫자가 바뀌는 변수는 사용자 코드에 없는 변수예요. p5.js 웹 에디터가 실행할 때마다 모든 for/while 루프에 "너무 오래 도는지 감시"하는 코드를 자동으로 주입하는데, 그때 쓰는 변수명이 _LP + 임의 번호입니다.
    blocks.js의 set_random_block() 함수(blocks.js:81-194)를 보면, switch문의 각 case마다 거의 동일한 구조의 2중 for문이 7번 반복되고 있어요. 에디터의 loop-protect 변환기가 이런 "switch + 반복되는 동일 구조 루프" 패턴을 만나면 주입한 감시 변수의 스코프를 잘못 잡아서 ReferenceError를 내는 알려진 버그가 있습니다 (2024~2026 사이 에디터가 내부적으로 loop-protect 라이브러리를 업데이트하면서 이 케이스가 새로 깨진 것으로 보입니다).
    게임 로직 코드 자체는 안 변했다는 게 맞고, 에디터 인프라 쪽 변화가 원인입니다.
    해결책: 7개의 거의 동일한 case 블록을 블록 모양을 변수로 선택해서 루프 하나로 합치면, 이 변환기가 헷갈릴 패턴 자체가 없어져서 우회됩니다. 로컬 저장소의 blocks.js도 같은 구조라 여기서 먼저 고쳐드릴까요? 고친 코드를 그대로 p5.js 에디터에 복사해 넣으면 바로 해결될 겁니다.

--- blocks.js
+++ blocks.js
@@ -78,113 +78,36 @@ for (let i = 0; i < block_component_num; i++)
 }
 }
 
-function set_random_block() 
+const BLOCK_SHAPES = {
+  3: block_O,
+  4: block_I,
+  5: block_Z,
+  6: block_S,
+  7: block_L,
+  8: block_J,
+  9: block_T
+};
+
+function set_random_block()
 {
   let index = 0; //  Assign xpos and ypos to each block.
   {
     block_type = next_block_num;
+    const shape = BLOCK_SHAPES[block_type];
 
-    switch (block_type) {
-      case 3:
-        for (let i = 0; i < 4; i++)
+    for (let i = 0; i < 4; i++)
+    {
+      for (let j = 0; j < 4; j++)
+      {
+        if (shape[i][j] == 1)
         {
-          for (let j = 0; j < 4; j++) 
-          {
-            if (block_O[i][j] == 1) 
-            {
-              blocks[index].xpos = j;
-              blocks[index].ypos = i;
-              index++;
-            }
-          }
+          blocks[index].xpos = j;
+          blocks[index].ypos = i;
+          index++;
         }
-        break;
-      case 4:
-        for (let i = 0; i < 4; i++)
-        {
-          for (let j = 0; j < 4; j++)
-          {
-            if (block_I[i][j] == 1) 
-            {
-              blocks[index].xpos = j;
-              blocks[index].ypos = i;
-              index++;
-            }
-          }
-        }
-        break;
-      case 5:
-        for (let i = 0; i < 4; i++) 
-        {
-          for (let j = 0; j < 4; j++) 
-          {
-            if (block_Z[i][j] == 1) 
-            {
-              blocks[index].xpos = j;
-              blocks[index].ypos = i;
-              index++;
-            }
-          }
-        }
-        break;
-      case 6:
-        for (let i = 0; i < 4; i++) 
-        {
-          for (let j = 0; j < 4; j++)
-          {
-            if (block_S[i][j] == 1) 
-            {
-              blocks[index].xpos = j;
-              blocks[index].ypos = i;
-              index++;
-            }
-          }
-        }
-        break;
-      case 7:
-        for (let i = 0; i < 4; i++) 
-        {
-          for (let j = 0; j < 4; j++) 
-          {
-            if (block_L[i][j] == 1)
-            {
-              blocks[index].xpos = j;
-              blocks[index].ypos = i;
-              index++;
-            }
-          }
-        }
-        break;
-      case 8:
-        for (let i = 0; i < 4; i++) 
-        {
-          for (let j = 0; j < 4; j++)
-          {
-            if (block_J[i][j] == 1) 
-            {
-              blocks[index].xpos = j;
-              blocks[index].ypos = i;
-              index++;
-            }
-          }
-        }
-        break;
-      case 9:
-        for (let i = 0; i < 4; i++)
-        {
-          for (let j = 0; j < 4; j++)
-          {
-            if (block_T[i][j] == 1) 
-            {
-              blocks[index].xpos = j;
-              blocks[index].ypos = i;
-              index++;
-            }
-          }
-        }
-        break;
+      }
     }
-    for (let i = 0; i < block_component_num; i++) 
+    for (let i = 0; i < block_component_num; i++)
     {
       blocks[i].xpos += 4;    //make xpos to ceter of board
     }
